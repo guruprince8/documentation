63bdc87159c71c5e8d761cbd41241c8c6f7cf644
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2023-08-10 23:47:48 -0500

    replaced comments with /*

--- database/postgres/functions.sql
+++ database/postgres/functions.sql
@@ -1,11 +1,12 @@
--- create or replace function totalRecords()
--- returns integer as $total$
--- declare 
--- 	total integer;
--- begin
--- 	select count(*) into total from public.company;
--- 	return total;
--- end;
--- $total$ language plpgsql;
+/*create or replace function totalRecords()
+returns integer as $total$
+declare 
+	total integer;
+begin
+	select count(*) into total from public.company;
+	return total;
+end;
+$total$ language plpgsql;
+*/
 
 select totalRecords()
