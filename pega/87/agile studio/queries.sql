596d4e761453ea2dc613389b6171c10c1278b199
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2023-08-14 19:11:49 -0500

    Create queries.sql
    added queries

---
+++ pega/87/agile studio/queries.sql
@@ -0,0 +1,9 @@
+-- Product (PegaProjMgmt-Work-Product)
+
+SELECT *
+FROM DATA_88.PPM_PRODUCT
+WHERE PYID = 'PRD-1004';
+
+--PegaProjMgmt-Work-Product-Version
+SELECT *
+FROM DATA_88.PPM_PRODUCT_VERSION;
