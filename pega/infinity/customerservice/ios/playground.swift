72d16426eebb585a783646c1e6ad1294dbffa6f5
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2023-10-17 21:37:04 -0500

    Create playground.swift
    import UIKit
    
    var greeting = "Hello, playground"
    print(greeting)
    print("Hello World")
    var myVariable = 42
    myVariable = 50
    let myConstant = 10
    print(myConstant)
    //let myConstant = 11
    let label = "The width is "
    let width = 94
    let widthLabel = label+String(width)
    print(widthLabel)

---
+++ pega/infinity/customerservice/ios/playground.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+print("Hello World")
+var myVariable = 42
+myVariable = 50
+let myConstant = 10
+print(myConstant)
+//let myConstant = 11
+let label = "The width is "
+let width = 94
+let widthLabel = label+String(width)
+print(widthLabel)
