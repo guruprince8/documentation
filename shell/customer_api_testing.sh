4357232d2df0cb97239f2b8213f496d1e07a3628
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2023-10-24 00:41:27 -0500

    Delete shell directory
    not needed

--- shell/customer_api_testing.sh
+++
@@ -1,8 +0,0 @@
-# date
-for (( i=0; i<10000; i++))
-do
-    echo `date`
-    curl http://localhost:3000/customer-management-service/v1/customers/40190957
-    # echo "\n"
-    echo "\n `date` "
-done    
