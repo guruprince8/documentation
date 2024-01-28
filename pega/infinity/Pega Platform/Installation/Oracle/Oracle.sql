43cf78124df55fdfd9d83dbde9c889dcf18975cb
guruprince8 <47411355+guruprince8@users.noreply.github.com>
2024-01-28 13:22:30 -0600

    Update Oracle.sql
    added initial logic...

--- pega/infinity/Pega Platform/Installation/Oracle/Oracle.sql
+++ pega/infinity/Pega Platform/Installation/Oracle/Oracle.sql
@@ -1,1 +1,22 @@
-/* set up schemas */
+
+/* https://stackoverflow.com/questions/33330968/error-ora-65096-invalid-common-user-or-role-name-in-oracle-database */
+alter session set "_ORACLE_SCRIPT"=true; 
+
+/* Set up access */
+CREATE USER RULES_23 IDENTIFIED BY oracle DEFAULT TABLESPACE USERS quota unlimited on USERS ;
+CREATE USER DATA_23 IDENTIFIED BY oracle DEFAULT TABLESPACE USERS quota unlimited on USERS;
+CREATE USER CUSTOMER_23 IDENTIFIED BY oracle DEFAULT TABLESPACE USERS quota unlimited on USERS;
+
+/* Grant inherit privileges */
+GRANT INHERIT ANY PRIVILEGES TO RULES_23;
+GRANT INHERIT ANY PRIVILEGES TO DATA_23;
+GRANT INHERIT ANY PRIVILEGES TO CUSTOMER_23;
+
+/* SQL Output
+User RULES_23 created.
+User DATA_23 created.
+User CUSTOMER_23 created.
+Grant succeeded.
+Grant succeeded.
+Grant succeeded.
+*/
